fe5799e0b4f94d5b62c9376f9003038d43c38c63
Andre Wiggins <andrewiggins@live.com>
2022-10-05 20:01:09 -0700

    Add initial vite setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # finding-friends
+
 A website to help play the trick-taking card game zhǎo péngyǒu (找朋友, 'Finding Friends')
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { render } from "preact";
+import "./index.css";
+
+const root = document.getElementById("root");
+render(<div>Hello World!</div>, root);
